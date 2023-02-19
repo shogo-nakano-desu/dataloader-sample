6cea57e5b070f53b01e410aace2f2edfc108e612
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2023-02-19 11:35:56 +0900

    feat: set initial sql for explanation

--- migration/init.sql
+++ migration/init.sql
@@ -38,20 +38,19 @@ INSERT INTO
 VALUES
     (4, 'pen', 2);
 
+-- INSERT INTO
+--     item
+-- VALUES
+--     (5, 'pinapple', 2);
 INSERT INTO
     item
 VALUES
-    (5, 'pinapple', 2);
+    (5, 'macbook pro', 3);
 
 INSERT INTO
     item
 VALUES
-    (6, 'macbook pro', 3);
-
-INSERT INTO
-    item
-VALUES
-    (7, 'hhkb', 3);
+    (6, 'hhkb', 3);
 
 -- Store
 INSERT INTO
@@ -72,27 +71,47 @@ VALUES
 INSERT INTO
     store
 VALUES
-    (4, 'shibuya stream', 3);
+    (4, 'tokyu store', 2);
 
 INSERT INTO
     store
 VALUES
-    (5, 'family mart', 4);
+    (5, 'shibuya stream', 3);
 
 INSERT INTO
     store
 VALUES
-    (6, 'united arrows', 5);
+    (6, 'tokyu hands', 3);
 
 INSERT INTO
     store
 VALUES
-    (7, 'amazon', 6);
+    (7, 'family mart', 4);
 
 INSERT INTO
     store
 VALUES
-    (8, 'rakuten', 7);
+    (8, 'ministop', 4);
+
+INSERT INTO
+    store
+VALUES
+    (9, 'united arrows', 5);
+
+INSERT INTO
+    store
+VALUES
+    (10, 'beams', 5);
+
+INSERT INTO
+    store
+VALUES
+    (11, 'amazon', 6);
+
+INSERT INTO
+    store
+VALUES
+    (12, 'rakuten', 6);
 
 ----------------
 --Author, Book, Version
@@ -120,32 +139,29 @@ VALUES
     (1, 'The Awakening', 1);
 
 INSERT INTO
-    book VALUE (2, 'A Night IN Acadie', 1);
+    book
+VALUES
+    (2, 'A Night IN Acadie', 1);
 
 INSERT INTO
     book
 VALUES
-    (3, 'Bayou Folk', 1);
+    (3, 'City of Glass', 2);
 
 INSERT INTO
     book
 VALUES
-    (4, 'City of Glass', 2);
+    (4, 'Squeeze Play', 2);
 
 INSERT INTO
     book
 VALUES
-    (5, 'Squeeze Play', 2);
+    (5, '1Q84', 3);
 
 INSERT INTO
     book
 VALUES
-    (6, '1Q84', 3);
-
-INSERT INTO
-    book
-VALUES
-    (7, 'Norwegian Wood', 3);
+    (6, 'Norwegian Wood', 3);
 
 -- Version
 INSERT INTO
@@ -161,37 +177,37 @@ VALUES
 INSERT INTO
     version
 VALUES
-    (3, 3, 1);
+    (3, 1, 2);
 
 INSERT INTO
     version
 VALUES
-    (4, 1, 2);
+    (4, 2, 2);
 
 INSERT INTO
     version
 VALUES
-    (5, 2, 2);
+    (5, 1, 3);
 
 INSERT INTO
     version
 VALUES
-    (6, 1, 3);
+    (6, 2, 3);
 
 INSERT INTO
     version
 VALUES
-    (7, 2, 3);
+    (7, 1, 4);
 
 INSERT INTO
     version
 VALUES
-    (8, 3, 3);
+    (8, 1, 4);
 
 INSERT INTO
     version
 VALUES
-    (9, 1, 4);
+    (9, 2, 5);
 
 INSERT INTO
     version
@@ -201,19 +217,9 @@ VALUES
 INSERT INTO
     version
 VALUES
-    (11, 2, 5);
+    (11, 2, 6);
 
 INSERT INTO
     version
 VALUES
     (12, 1, 6);
-
-INSERT INTO
-    version
-VALUES
-    (13, 2, 6);
-
-INSERT INTO
-    version
-VALUES
-    (14, 1, 7);
